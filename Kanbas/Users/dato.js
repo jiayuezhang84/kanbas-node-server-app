3018a88238385a756844b945517f2af1f82fbdec
jiayuezhang84 <josiezhang84@gmail.com>
2024-07-21 01:38:29 -0400

    a6

---
+++ Kanbas/Users/dato.js
@@ -0,0 +1,6 @@
+export const findUsersByPartialName = (partialName) => {
+    const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
+    return model.find({
+      $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
+    });
+  };
