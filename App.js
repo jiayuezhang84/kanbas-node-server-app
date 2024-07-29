bc254550cd4d920a15e17f741ec4671036db7ab6
jiayuezhang84 <josiezhang84@gmail.com>
2024-07-29 13:05:55 +0200

    a6 updates

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
+// nodemon  App.js
 import "dotenv/config";
 import express from 'express'
+import mongoose from "mongoose";
 import mongoose from "mongoose";
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
@@ -8,32 +10,14 @@ import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from './Kanbas/Assignments/routes.js';
 import cors from "cors";
 import UserRoutes from "./Kanbas/Users/routes.js";
-import session from "express-session";
 
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
 mongoose.connect(CONNECTION_STRING);
   
-const app = express();
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:5173",
-  })
-);
-const sessionOptions = {
-  secret: process.env.SESSION_SECRET || "kanbas",
-  resave: false,
-  saveUninitialized: false,
-};
-if (process.env.NODE_ENV !== "development") {
-  sessionOptions.proxy = true;
-  sessionOptions.cookie = {
-    sameSite: "none",
-    secure: true,
-    domain: process.env.NODE_SERVER_DOMAIN,
-  };
-}
-app.use(session(sessionOptions));
+const app = express()
+
+app.use(cors());
+app.use(express.json());
 
 UserRoutes(app);
 CourseRoutes(app);
@@ -41,5 +25,6 @@ ModuleRoutes(app);
 AssignmentRoutes(app);
 Lab5(app)
 Hello(app)
+
 
 app.listen(process.env.PORT || 4000)
