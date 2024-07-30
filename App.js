0e6d0b62403eb5a9872ee481b07b5f4fcd5d0b55
jiayuezhang84 <josiezhang84@gmail.com>
2024-07-30 23:33:36 +0200

    remove-useNewUrlParser

--- App.js
+++ App.js
@@ -1,4 +1,52 @@
 // nodemon App.js
+// import "dotenv/config";
+// import express from 'express'
+// import mongoose from "mongoose";
+// import Hello from "./Hello.js"
+// import Lab5 from "./Lab5/index.js";
+// import CourseRoutes from "./Kanbas/Courses/routes.js";
+// import ModuleRoutes from "./Kanbas/Modules/routes.js";
+// import AssignmentRoutes from './Kanbas/Assignments/routes.js';
+// import cors from "cors";
+// import UserRoutes from "./Kanbas/Users/routes.js";
+// import session from "express-session";
+
+// const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+// mongoose.connect(CONNECTION_STRING);
+  
+// const app = express()
+
+// app.use(
+//  cors({
+//    credentials: true,
+//    origin: process.env.NETLIFY_URL || "http://localhost:5173",
+//  })
+// );
+// app.use(express.json())
+// const sessionOptions = {
+//     secret: process.env.SESSION_SECRET || "kanbas",
+//     resave: false,
+//     saveUninitialized: false,
+//   };
+//   if (process.env.NODE_ENV !== "development") {
+//     sessionOptions.proxy = true;
+//     sessionOptions.cookie = {
+//       sameSite: "none",
+//       secure: true,
+//       domain: process.env.NODE_SERVER_DOMAIN,
+//     };
+//   }
+//   app.use(session(sessionOptions));
+  
+// UserRoutes(app);
+// CourseRoutes(app);
+// ModuleRoutes(app);
+// AssignmentRoutes(app);
+// Lab5(app)
+// Hello(app)
+
+
+// app.listen(process.env.PORT || 4000)
 import "dotenv/config";
 import express from 'express';
 import mongoose from "mongoose";
@@ -14,10 +62,7 @@ import MongoStore from 'connect-mongo';
 
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
 
-mongoose.connect(CONNECTION_STRING, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(CONNECTION_STRING).then(() => {
   console.log('Connected to MongoDB');
 }).catch(err => {
   console.error('Failed to connect to MongoDB', err);
@@ -64,3 +109,4 @@ Hello(app);
 app.listen(process.env.PORT || 4000, () => {
   console.log(`Server is running on port ${process.env.PORT || 4000}`);
 });
+
